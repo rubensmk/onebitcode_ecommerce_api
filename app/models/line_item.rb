e016ba9dc6e9f7d43189d2f8c1d92268969daae4
rubensmk <rubenskishimoto@gmail.com>
2023-04-28 21:22:23 -0300

    Add new services and fields

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,11 +1,24 @@
 class LineItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+  has_many :licenses
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :payed_price, presence: true, numericality: { greater_than: 0 }
+  validates :status, presence: true, on: :update
 
+  enum status: { waiting_order: 1, preparing: 2, en_route: 3, delivered: 4 }
+
+  before_validation :set_default_status, on: :create
+  
   def total
     payed_price * quantity
   end
+
+  private
+
+  def set_default_status
+    self.status = :waiting_order
+  end
+
 end
